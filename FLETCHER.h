78a637289b7b738a63efea30b9c9b5af32fc484e
Jonathan Peri <97846999+periiDevl@users.noreply.github.com>
2026-09-13 03:53:58 +0300

    REFACTOR 1

--- FLETCHER.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<stdint.h>
-#include<stdio.h>
-
-uint16_t fletcher16(const uint8_t *data, size_t len);
--- SimpleDataStructs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<stdlib.h>
-
-struct LinkedList
-{
-    void* data;
-    void* next;  
-};
